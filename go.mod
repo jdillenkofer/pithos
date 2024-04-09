91e4ce8edb5ca7910fe54447c55b58cbac68a090
Jan Dillenkofer <Jan.Dillenkofer@t-online.de>
2024-04-09 21:41:31 +0200

    feat: Use golang-migrate for db migrations

--- go.mod
+++ go.mod
@@ -3,7 +3,8 @@ module github.com/jdillenkofer/pithos
 go 1.22
 
 require (
-	github.com/aws/aws-sdk-go v1.51.16
+	github.com/aws/aws-sdk-go v1.51.18
+	github.com/golang-migrate/migrate/v4 v4.17.0
 	github.com/mattn/go-sqlite3 v1.14.22
 	github.com/oklog/ulid/v2 v2.1.0
 	github.com/stretchr/testify v1.9.0
@@ -11,7 +12,10 @@ require (
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	go.uber.org/atomic v1.11.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ migrations/1_initial.up.sql
@@ -0,0 +1,28 @@
+CREATE TABLE buckets (
+  id TEXT NOT NULL PRIMARY KEY,
+  name TEXT NOT NULL UNIQUE,
+  created_at DATETIME NOT NULL,
+  updated_at DATETIME NOT NULL
+);
+
+CREATE TABLE objects (
+  id TEXT NOT NULL primary key,
+  bucket_name TEXT NOT NULL,
+  key TEXT NOT NULL,
+  etag TEXT NOT NULL,
+  size INTEGER NOT NULL,
+  created_at DATETIME NOT NULL,
+  updated_at DATETIME NOT NULL,
+  FOREIGN KEY(bucket_name) REFERENCES buckets(name),
+  UNIQUE(bucket_name, key)
+);
+
+CREATE TABLE blobs (
+  id TEXT NOT NULL primary key,
+  object_id TEXT NOT NULL,
+  sequence_number INTEGER NOT NULL,
+  created_at DATETIME NOT NULL,
+  updated_at DATETIME NOT NULL,
+  FOREIGN KEY(object_id) REFERENCES objects(id),
+  UNIQUE(object_id, sequence_number)
+);
