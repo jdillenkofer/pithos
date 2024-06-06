2fb83d0ee22a2cd9f40b83a7e374a876d815362c
Jan Dillenkofer <Jan.Dillenkofer@t-online.de>
2024-06-06 16:16:22 +0200

    test: Add MergeSettings test

---
+++ internal/settings/settings_test.go
@@ -0,0 +1,50 @@
+package settings
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func addrOf[T any](t T) *T { return &t }
+
+func TestMergeSettingsTwoNils(t *testing.T) {
+	a := Settings{
+		domain: nil,
+	}
+	b := Settings{
+		domain: nil,
+	}
+	mergedSettings := mergeSettings(&a, &b)
+	assert.NotNil(t, mergedSettings)
+	assert.Nil(t, a.domain)
+	assert.Nil(t, b.domain)
+	assert.Nil(t, mergedSettings.domain)
+}
+func TestMergeSettingsNilAndValue(t *testing.T) {
+	a := Settings{
+		domain: nil,
+	}
+	b := Settings{
+		domain: addrOf("test"),
+	}
+	mergedSettings := mergeSettings(&a, &b)
+	assert.NotNil(t, mergedSettings)
+	assert.Nil(t, a.domain)
+	assert.Equal(t, "test", *b.domain)
+	assert.Equal(t, b.domain, mergedSettings.domain)
+}
+
+func TestMergeSettingsTwoValues(t *testing.T) {
+	a := Settings{
+		domain: addrOf("test"),
+	}
+	b := Settings{
+		domain: addrOf("test2"),
+	}
+	mergedSettings := mergeSettings(&a, &b)
+	assert.NotNil(t, mergedSettings)
+	assert.Equal(t, "test", *a.domain)
+	assert.Equal(t, "test2", *b.domain)
+	assert.Equal(t, b.domain, mergedSettings.domain)
+}
