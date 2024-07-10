24307aff725909d06df49991020d01790fb41f67
Jan Dillenkofer <Jan.Dillenkofer@t-online.de>
2024-07-10 19:18:58 +0200

    test: Add tests for database migrations

---
+++ internal/storage/database_test.go
@@ -0,0 +1,84 @@
+package storage
+
+import (
+	"database/sql"
+	"testing"
+
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/sqlite3"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	"github.com/golang-migrate/migrate/v4/source/iofs"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMigrateUp(t *testing.T) {
+	db, err := sql.Open("sqlite3", ":memory:")
+	assert.Nil(t, err)
+
+	sourceDriver, err := iofs.New(migrationsFilesystem, "migrations")
+	assert.Nil(t, err)
+
+	databaseDriver, err := sqlite3.WithInstance(db, &sqlite3.Config{})
+	assert.Nil(t, err)
+
+	m, err := migrate.NewWithInstance("iofs", sourceDriver, "sqlite3", databaseDriver)
+	assert.Nil(t, err)
+
+	err = m.Up()
+	if err != nil {
+		assert.Fail(t, err.Error())
+	}
+}
+
+func TestMigrateUpAndDown(t *testing.T) {
+	db, err := sql.Open("sqlite3", ":memory:")
+	assert.Nil(t, err)
+
+	sourceDriver, err := iofs.New(migrationsFilesystem, "migrations")
+	assert.Nil(t, err)
+
+	databaseDriver, err := sqlite3.WithInstance(db, &sqlite3.Config{})
+	assert.Nil(t, err)
+
+	m, err := migrate.NewWithInstance("iofs", sourceDriver, "sqlite3", databaseDriver)
+	assert.Nil(t, err)
+
+	err = m.Up()
+	if err != nil {
+		assert.Fail(t, err.Error())
+	}
+
+	err = m.Down()
+	if err != nil {
+		assert.Fail(t, err.Error())
+	}
+}
+
+func TestMigrateUpAndDownAndUp(t *testing.T) {
+	db, err := sql.Open("sqlite3", ":memory:")
+	assert.Nil(t, err)
+
+	sourceDriver, err := iofs.New(migrationsFilesystem, "migrations")
+	assert.Nil(t, err)
+
+	databaseDriver, err := sqlite3.WithInstance(db, &sqlite3.Config{})
+	assert.Nil(t, err)
+
+	m, err := migrate.NewWithInstance("iofs", sourceDriver, "sqlite3", databaseDriver)
+	assert.Nil(t, err)
+
+	err = m.Up()
+	if err != nil {
+		assert.Fail(t, err.Error())
+	}
+
+	err = m.Down()
+	if err != nil {
+		assert.Fail(t, err.Error())
+	}
+
+	err = m.Up()
+	if err != nil {
+		assert.Fail(t, err.Error())
+	}
+}
