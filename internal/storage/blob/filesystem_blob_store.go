6120468377c2f0f0cb09759bf12041b4020047c8
Jan Dillenkofer <Jan.Dillenkofer@t-online.de>
2024-11-07 13:54:03 +0100

    chore: Add TODO to outboxBlobStore

--- internal/storage/blob/filesystem_blob_store.go
+++ internal/storage/blob/filesystem_blob_store.go
@@ -92,7 +92,7 @@ func (bs *FilesystemBlobStore) GetBlob(ctx context.Context, tx *sql.Tx, blobId B
 func (bs *FilesystemBlobStore) GetBlobIds(ctx context.Context, tx *sql.Tx) ([]BlobId, error) {
 	return []BlobId{}, nil
 	// @TODO: implement me
-	// return nil, errors.New("not implemented yeterino")
+	// return nil, errors.New("not implemented yet")
 }
 
 func (bs *FilesystemBlobStore) DeleteBlob(ctx context.Context, tx *sql.Tx, blobId BlobId) error {
