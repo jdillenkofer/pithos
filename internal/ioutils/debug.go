eed8bef8e1162bf7f4acab0a7c734cb5793b2f2c
Jan Dillenkofer <Jan.Dillenkofer@t-online.de>
2025-01-12 12:04:04 +0100

    refactor: Allow NewDebugReadCloser to detect optional Seek

--- internal/ioutils/debug.go
+++ internal/ioutils/debug.go
@@ -11,6 +11,12 @@ type debugReadCloser struct {
 }
 
 func NewDebugReadCloser(name string, innerReadCloser io.ReadCloser) io.ReadCloser {
+	if innerReadSeekCloser, ok := innerReadCloser.(io.ReadSeekCloser); ok {
+		return &debugReadSeekCloser{
+			name:                name,
+			innerReadSeekCloser: innerReadSeekCloser,
+		}
+	}
 	return &debugReadCloser{
 		name:            name,
 		innerReadCloser: innerReadCloser,
@@ -18,9 +24,9 @@ func NewDebugReadCloser(name string, innerReadCloser io.ReadCloser) io.ReadClose
 }
 
 func (d *debugReadCloser) Read(p []byte) (int, error) {
-	log.Println("DebugReadCloser", d.name, "Read(", p, ")")
+	log.Println("DebugReadSeekCloser", d.name, "Read(", p, ")")
 	n, err := d.innerReadCloser.Read(p)
-	log.Println("DebugReadCloser", d.name, "Read(", p, ") = (n: ", n, ", err:", err, "); p:", "\""+string(p[0:n])+"\"")
+	log.Println("DebugReadSeekCloser", d.name, "Read(", p, ") = (n: ", n, ", err:", err, ")")
 	return n, err
 }
 
@@ -28,5 +34,31 @@ func (d *debugReadCloser) Close() error {
 	log.Println("DebugReadCloser", d.name, "Close()")
 	err := d.innerReadCloser.Close()
 	log.Println("DebugReadCloser", d.name, "Close() = err: ", err)
+	return err
+}
+
+type debugReadSeekCloser struct {
+	name                string
+	innerReadSeekCloser io.ReadSeekCloser
+}
+
+func (d *debugReadSeekCloser) Read(p []byte) (int, error) {
+	log.Println("DebugReadSeekCloser", d.name, "Read(", p, ")")
+	n, err := d.innerReadSeekCloser.Read(p)
+	log.Println("DebugReadSeekCloser", d.name, "Read(", p, ") = (n: ", n, ", err:", err, ")")
+	return n, err
+}
+
+func (d *debugReadSeekCloser) Seek(offset int64, whence int) (int64, error) {
+	log.Println("DebugReadSeekCloser", d.name, "Seek(", offset, ",", whence, ")")
+	n, err := d.innerReadSeekCloser.Seek(offset, whence)
+	log.Println("DebugReadSeekCloser", d.name, "Seek(", offset, ",", whence, ") = (n: ", n, ", err:", err, ")")
+	return n, err
+}
+
+func (d *debugReadSeekCloser) Close() error {
+	log.Println("DebugReadSeekCloser", d.name, "Close()")
+	err := d.innerReadSeekCloser.Close()
+	log.Println("DebugReadSeekCloser", d.name, "Close() = err: ", err)
 	return err
 }
