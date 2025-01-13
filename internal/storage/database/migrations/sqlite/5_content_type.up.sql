956bada631a8435b19224f39aa361b5f4e437668
Jan Dillenkofer <Jan.Dillenkofer@t-online.de>
2025-01-13 16:43:51 +0100

    fix(migration): Set default values for content_type column

--- internal/storage/database/migrations/sqlite/5_content_type.up.sql
+++ internal/storage/database/migrations/sqlite/5_content_type.up.sql
@@ -1,2 +1,2 @@
-ALTER TABLE storage_outbox_entries ADD COLUMN content_type TEXT NOT NULL;
-ALTER TABLE objects ADD COLUMN content_type TEXT NOT NULL;
+ALTER TABLE storage_outbox_entries ADD COLUMN content_type TEXT NOT NULL DEFAULT "";
+ALTER TABLE objects ADD COLUMN content_type TEXT NOT NULL DEFAULT "";
