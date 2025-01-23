ca08109c91d2534e11d88d1e193b960d72c98120
Jan Dillenkofer <Jan.Dillenkofer@t-online.de>
2025-01-23 20:13:25 +0100

    test: Add a small sleep before calling funcs on the  evictionPolicy

--- internal/storage/cache/evictionpolicy/evictionpolicy_test.go
+++ internal/storage/cache/evictionpolicy/evictionpolicy_test.go
@@ -2,6 +2,7 @@ package evictionpolicy_test
 
 import (
 	"testing"
+	"time"
 
 	"github.com/jdillenkofer/pithos/internal/storage/cache/evictionpolicy/evictionchecker/fixedkeylimit"
 	"github.com/jdillenkofer/pithos/internal/storage/cache/evictionpolicy/evictionchecker/fixedsizelimit"
@@ -45,30 +46,43 @@ func TestLFUCacheEvictionPolicy(t *testing.T) {
 	ec, err := fixedsizelimit.New(3)
 	assert.Nil(t, err)
 
+	// The lfuCacheEvictionPolicy is sensitive to timing.
+	// The time package uses a monotonic clock,
+	// but it is allowed to return a value equal to the time,
+	// we read in the last time.Now() call.
 	lfuCacheEvictionPolicy, err := lfu.New(ec)
 	assert.Nil(t, err)
 
 	assert.Empty(t, lfuCacheEvictionPolicy.TrackSetAndReturnEvictedKeys("a", []byte{0x0}))
+	time.Sleep(200 * time.Millisecond)
 	assert.Empty(t, lfuCacheEvictionPolicy.TrackSetAndReturnEvictedKeys("b", []byte{0x1}))
+	time.Sleep(200 * time.Millisecond)
 	assert.Empty(t, lfuCacheEvictionPolicy.TrackSetAndReturnEvictedKeys("c", []byte{0x2}))
+	time.Sleep(200 * time.Millisecond)
 	lfuCacheEvictionPolicy.TrackGet("a")
+	time.Sleep(200 * time.Millisecond)
 	lfuCacheEvictionPolicy.TrackGet("b")
+	time.Sleep(200 * time.Millisecond)
 	lfuCacheEvictionPolicy.TrackGet("c")
 
+	time.Sleep(200 * time.Millisecond)
 	evictedKeys := lfuCacheEvictionPolicy.TrackSetAndReturnEvictedKeys("d", []byte{0x0})
 	assert.Equal(t, 1, len(evictedKeys))
 	assert.Equal(t, "a", evictedKeys[0])
 	lfuCacheEvictionPolicy.TrackGet("d")
 
+	time.Sleep(200 * time.Millisecond)
 	evictedKeys2 := lfuCacheEvictionPolicy.TrackSetAndReturnEvictedKeys("e", []byte{0x0})
 	assert.Equal(t, 1, len(evictedKeys2))
 	assert.Equal(t, "b", evictedKeys2[0])
 	lfuCacheEvictionPolicy.TrackGet("e")
 
+	time.Sleep(200 * time.Millisecond)
 	evictedKeys3 := lfuCacheEvictionPolicy.TrackSetAndReturnEvictedKeys("f", []byte{0x0})
 	assert.Equal(t, 1, len(evictedKeys3))
 	assert.Equal(t, "c", evictedKeys3[0])
 
+	time.Sleep(200 * time.Millisecond)
 	evictedKeys4 := lfuCacheEvictionPolicy.TrackSetAndReturnEvictedKeys("g", []byte{0x0})
 	assert.Equal(t, 1, len(evictedKeys4))
 	assert.Equal(t, "f", evictedKeys4[0])
