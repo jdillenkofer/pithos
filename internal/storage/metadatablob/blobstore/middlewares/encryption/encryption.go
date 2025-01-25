c75d6d088462ffd98cea8d11bfde5140437442c4
Jan Dillenkofer <Jan.Dillenkofer@t-online.de>
2025-01-25 16:33:22 +0100

    feat: Implement streaming encryption

--- internal/storage/metadatablob/blobstore/middlewares/encryption/encryption.go
+++ internal/storage/metadatablob/blobstore/middlewares/encryption/encryption.go
@@ -52,40 +52,125 @@ func (ebsm *encryptionBlobStoreMiddleware) Stop(ctx context.Context) error {
 	return ebsm.innerBlobStore.Stop(ctx)
 }
 
-func (ebsm *encryptionBlobStoreMiddleware) PutBlob(ctx context.Context, tx *sql.Tx, blobId blobstore.BlobId, reader io.Reader) error {
-	// @TODO: streaming encryption and padding
-	data, err := io.ReadAll(reader)
+type cbcEncrypterReader struct {
+	mode                        cipher.BlockMode
+	innerReadCloser             io.ReadCloser
+	internalSrc                 []byte
+	internalDst                 []byte
+	bytesRemainingInInternalDst int64
+	reachedEOF                  bool
+}
+
+func newCBCEncrypterReader(key []byte, iv []byte, innerReadCloser io.ReadCloser) (io.ReadCloser, error) {
+	block, err := aes.NewCipher(key)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
+	mode := cipher.NewCBCEncrypter(block, iv)
+	// @Note(ExtraBlocks): We add two extraBlocks for padding and buf extension in second ReadFull call
+	const extraBlocks = 2
+	return &cbcEncrypterReader{
+		mode:                        mode,
+		innerReadCloser:             innerReadCloser,
+		internalSrc:                 make([]byte, aes.BlockSize*512),
+		internalDst:                 make([]byte, aes.BlockSize*(512+extraBlocks)),
+		bytesRemainingInInternalDst: 0,
+		reachedEOF:                  false,
+	}, nil
+}
+
+func (dr *cbcEncrypterReader) Read(p []byte) (int, error) {
+	if dr.bytesRemainingInInternalDst > 0 {
+		n := copy(p, dr.internalDst[:dr.bytesRemainingInInternalDst])
+		copy(dr.internalDst, dr.internalDst[n:dr.bytesRemainingInInternalDst])
+		dr.bytesRemainingInInternalDst -= int64(n)
+		return n, nil
+	}
+	// Make sure that no more data can be read when reaching endOffset
+	if dr.reachedEOF {
+		return 0, io.EOF
+	}
+
+	var bytesRead int
+	var err error
+	// Read at least aes.BlockSize bytes
+	bytesRead, err = io.ReadAtLeast(dr.innerReadCloser, dr.internalSrc, aes.BlockSize)
+	if err == io.ErrUnexpectedEOF || err == io.EOF {
+		paddedData, err := pkcs7padding.Pad(dr.internalSrc[:bytesRead], aes.BlockSize)
+		if err != nil {
+			return -1, err
+		}
+		dr.reachedEOF = true
+		// @Note(ExtraBlocks): After this internalSrc may be bigger than 512*aes.Blocksize
+		// We make sure that there is enough room in internalDst by adding two extra aes.Blocksize many bytes
+		dr.internalSrc = paddedData
+		bytesRead = len(paddedData)
+	} else if err != nil {
+		return bytesRead, err
+	}
+
+	// Always round up to the next full blockSize
+	remainingBytesToReadForFullBlock := bytesRead % aes.BlockSize
+	if remainingBytesToReadForFullBlock > 0 {
+		if dr.reachedEOF {
+			return -1, ErrPlaintextNotAMultipleOfBlockSize
+		}
+		buf := make([]byte, remainingBytesToReadForFullBlock)
+		bytesRead2, err := io.ReadFull(dr.innerReadCloser, buf)
+		if err == io.ErrUnexpectedEOF || err == io.EOF {
+			bytesCopied := copy(dr.internalSrc[bytesRead:], buf[:bytesRead2])
+			if bytesRead2 != bytesCopied {
+				panic("cbcEncrypterReader(1): Copy did not copy all bytes")
+			}
+			bytesRead += bytesRead2
+
+			// @Note(ExtraBlocks): After this internalSrc may be bigger than 512*aes.Blocksize
+			// We make sure that there is enough room in internalDst by adding two extra aes.Blocksize many bytes
+			paddedData, err := pkcs7padding.Pad(dr.internalSrc[:bytesRead], aes.BlockSize)
+			if err != nil {
+				return -1, err
+			}
+			dr.reachedEOF = true
+			dr.internalSrc = paddedData
+			bytesRead = len(paddedData)
+		} else if err != nil {
+			return bytesRead2, err
+		} else {
+			bytesCopied := copy(dr.internalSrc[bytesRead:], buf[:bytesRead2])
+			if bytesRead2 != bytesCopied {
+				panic("cbcEncrypterReader(2): Copy did not copy all bytes")
+			}
+			bytesRead += bytesRead2
+		}
+	}
+
+	dr.mode.CryptBlocks(dr.internalDst[:bytesRead], dr.internalSrc[:bytesRead])
+	dr.bytesRemainingInInternalDst += int64(bytesRead)
+
+	n := copy(p, dr.internalDst[:dr.bytesRemainingInInternalDst])
+	copy(dr.internalDst, dr.internalDst[n:dr.bytesRemainingInInternalDst])
+	dr.bytesRemainingInInternalDst -= int64(n)
+	return n, nil
+}
+
+func (dr *cbcEncrypterReader) Close() error {
+	return dr.innerReadCloser.Close()
+}
+
+func (ebsm *encryptionBlobStoreMiddleware) PutBlob(ctx context.Context, tx *sql.Tx, blobId blobstore.BlobId, reader io.Reader) error {
 	// Generate a random initialization vector
 	iv := make([]byte, aes.BlockSize)
 	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
 		return err
 	}
 
-	block, err := aes.NewCipher(ebsm.key)
+	ciphertextReadCloser, err := newCBCEncrypterReader(ebsm.key, iv, io.NopCloser(reader))
 	if err != nil {
 		return err
 	}
-
-	mode := cipher.NewCBCEncrypter(block, iv)
-
-	paddedData, err := pkcs7padding.Pad(data, aes.BlockSize)
-	if err != nil {
-		return err
-	}
-
-	if len(paddedData)%aes.BlockSize != 0 {
-		return ErrPlaintextNotAMultipleOfBlockSize
-	}
-
-	ciphertext := make([]byte, len(paddedData))
-	mode.CryptBlocks(ciphertext, paddedData)
 
 	ivReadCloser := ioutils.NewByteReadSeekCloser(iv)
-	ciphertextReadCloser := ioutils.NewByteReadSeekCloser(ciphertext)
 	var ivAndCiphertextReadCloser io.ReadCloser = ioutils.NewMultiReadCloser(ivReadCloser, ciphertextReadCloser)
 
 	{
@@ -220,7 +305,10 @@ func (dr *cbcDecrypterReader) Read(p []byte) (int, error) {
 			if err != nil {
 				return bytesRead2, err
 			}
-			copy(dr.internalSrc[bytesRead:], buf)
+			bytesCopied := copy(dr.internalSrc[bytesRead:], buf)
+			if len(buf) != bytesCopied {
+				panic("cbcDecrypterReader: Copy did not copy all bytes")
+			}
 			bytesRead += bytesRead2
 		}
 	}
