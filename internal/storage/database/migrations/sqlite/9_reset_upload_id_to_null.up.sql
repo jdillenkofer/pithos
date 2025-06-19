4774b51505d61309ed19e913b73ac50ca37a0482
Jan Dillenkofer <Jan.Dillenkofer@t-online.de>
2025-06-19 15:39:10 +0200

    fix: Migration 9 not using correct null check

--- internal/storage/database/migrations/sqlite/9_reset_upload_id_to_null.up.sql
+++ internal/storage/database/migrations/sqlite/9_reset_upload_id_to_null.up.sql
@@ -1,4 +1,4 @@
 -- Set upload_id of every completed upload to NULL
 UPDATE objects
 SET upload_id = NULL
-WHERE upload_id != NULL AND upload_status = "COMPLETED";
+WHERE upload_id IS NOT NULL AND upload_status = "COMPLETED";
