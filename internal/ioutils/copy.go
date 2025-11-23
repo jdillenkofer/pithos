fa4dc72cb66ce573b08dba1e9858953ca32731b9
Jan Dillenkofer <Jan.Dillenkofer@t-online.de>
2025-11-23 11:31:04 +0100

    refactor: Increase buffering when copying to response body

---
+++ internal/ioutils/copy.go
@@ -0,0 +1,20 @@
+package ioutils
+
+import (
+	"io"
+)
+
+// CopyN copies n bytes (or until an error) from src to dst.
+// It uses io.CopyBuffer with a 128KB buffer.
+func CopyN(dst io.Writer, src io.Reader, n int64) (written int64, err error) {
+	buf := make([]byte, 128*1024)
+	written, err = io.CopyBuffer(dst, io.LimitReader(src, n), buf)
+	if written == n {
+		return n, nil
+	}
+	if written < n && err == nil {
+		// src stopped early; must have been EOF.
+		err = io.EOF
+	}
+	return
+}
