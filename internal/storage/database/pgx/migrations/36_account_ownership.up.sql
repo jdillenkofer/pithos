aeeac3260d3c66e1c19ee52e7c15e56e8de20ff7
Jan Dillenkofer <Jan.Dillenkofer@t-online.de>
2026-09-13 13:29:57 +0200

    fix(outbox): preserve legacy bucket ownership

--- internal/storage/database/pgx/migrations/36_account_ownership.up.sql
+++ internal/storage/database/pgx/migrations/36_account_ownership.up.sql
@@ -13,3 +13,5 @@ CREATE TABLE storage_outbox_entry_create_bucket_options (
   outbox_entry_id TEXT PRIMARY KEY REFERENCES storage_outbox_entries(id) ON DELETE CASCADE,
   owner_account_id TEXT NOT NULL
 );
+INSERT INTO storage_outbox_entry_create_bucket_options (outbox_entry_id, owner_account_id)
+SELECT id, 'legacy' FROM storage_outbox_entries WHERE operation = 'CreateBucket';
